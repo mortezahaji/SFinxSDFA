123d8910fff86e29f4e28183a0e9c254cd121df1
Morteza Hajihosseini <mortezahaji87@gmail.com~>
2026-09-05 16:18:48 -0600

    commit

--- DESCRIPTION.R
+++ DESCRIPTION.R
@@ -2,22 +2,23 @@ Package: SFinxSDFA
 Type: Package
 Title: Spatial Fingerprint Analytics and Spatial Functional Data Analysis
 Version: 0.1.0
-Authors@R: person("Morteza", "Hajihosseini", role = c("aut", "cre"), email = "mhajihos@ualberta.ca")
+Authors@R: person("Morteza", "Hajihosseini", role = c("aut", "cre"), email = "mortezahaji87@gmail.com")
 Description: R package for Spatial Fingerprints Analytics (SFinx) and Spatial Functional Data Analysis (SDFA) in spatial transcriptomics data. Reconstructs spatial landscapes, evaluates spatial operators, and fits spatially varying GAM regression models with tensor product splines.
+License: MIT + file LICENSE
 Encoding: UTF-8
 RoxygenNote: 7.2.3
 Imports:
-    ggplot2,
-    ggpubr,
-    dplyr,
-    mgcv,
-    automap,
-    gstat,
-    sf,
-    sp,
-    RANN,
-    plot3D,
-    bestNormalize,
-    data.table,
-    gridExtra,
-    viridis
+      ggplot2,
+      ggpubr,
+      dplyr,
+      mgcv,
+      automap,
+      gstat,
+      sf,
+      sp,
+      RANN,
+      plot3D,
+      bestNormalize,
+      data.table,
+      gridExtra,
+      viridis,
